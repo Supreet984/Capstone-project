24e9e2d4ffe25b53c56c7ef71ab202fad50a6142
Admin <65367030+Supreet984@users.noreply.github.com>
2023-04-22 03:36:26 +0545

    Create EmployeeControllerTest.java

---
+++ src/test/java/com/capstone/Capstone/project/Controller/EmployeeControllerTest.java
@@ -0,0 +1,43 @@
+package com.capstone.Capstone.project.Controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void home() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getEmployeeById() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+}
