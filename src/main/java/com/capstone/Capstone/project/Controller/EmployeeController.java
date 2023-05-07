8c1a1348780411bacc42d8c72a48d25cac61890a
Admin <65367030+Supreet984@users.noreply.github.com>
2023-05-07 12:52:29 +0545

    Update EmployeeController.java

--- src/main/java/com/capstone/Capstone/project/Controller/EmployeeController.java
+++ src/main/java/com/capstone/Capstone/project/Controller/EmployeeController.java
@@ -9,6 +9,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+
 @RestController
 @AllArgsConstructor
 public class EmployeeController {
@@ -37,42 +39,57 @@ public class EmployeeController {
     //get all
     @GetMapping("/employees")
     public ResponseEntity<?> getAllEmployees() {
-        logger.info("Get All Employees");
+        logger.info("Get All Employees"+ " " + new Date());
         return employeeService.getAllEmployees();
     }
 
     //get by id
     @GetMapping("/employees/{id}")
-    public ResponseEntity<?> getEmployeeById(@PathVariable Long id) {
-        logger.info("Get Employee By ID: " + id);
-        return employeeService.getEmployeeById(id);
+    public ResponseEntity<?> getEmployeeById(@PathVariable String id) {
+        try{
+            Long.parseLong(id);
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body("Invalid ID");
+        }
+        logger.info("Get Employee By ID: " + id + " " + new Date());
+        return employeeService.getEmployeeById(Long.parseLong(id));
     }
 
     //add
     @PostMapping("/employees/add")
     public ResponseEntity<?> addEmployee(@RequestBody Employee employee) throws Exception {
-        logger.info("Add Employee: " + employee.getEmployeeName() + " " + employee.getDateOfBirth());
+        logger.info("Add Employee: " + employee.getEmployeeName() + " " + employee.getDateOfBirth()+ " " + new Date());
         return employeeService.addEmployee(employee);
     }
 
     //update
     @PutMapping("/employees/update")
     public ResponseEntity<?> updateEmployee(@RequestBody Employee employee) {
-        logger.info("Update Employee: " + employee.getEmployeeName() + " " + employee.getDateOfBirth());
+        logger.info("Update Employee: " + employee.getEmployeeName() + " " + employee.getDateOfBirth()+ " " + new Date());
         return employeeService.updateEmployee(employee);
     }
 
     @DeleteMapping("/employees/delete/{id}")
-    public ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+    public ResponseEntity<?> deleteEmployee(@PathVariable String id) {
+        try{
+            Long.parseLong(id);
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body("Invalid ID");
+        }
         logger.info("Delete Employee: " + id);
-        return employeeService.deleteEmployee(id);
+        return employeeService.deleteEmployee(Long.parseLong(id));
     }
 
     //employees/byID?id=1
     @GetMapping("/employees/byID")
-    public ResponseEntity<?> getEmployeeByIdParam(@RequestParam Long id) {
-        logger.info("Get Employee By ID: " + id);
-        return employeeService.getEmployeeById(id);
+    public ResponseEntity<?> getEmployeeByIdParam(@RequestParam String id) {
+        try{
+            Long.parseLong(id);
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body("Invalid ID");
+        }
+        logger.info("Get Employee By ID: " + id + " " + new Date());
+        return employeeService.getEmployeeById(Long.parseLong(id));
     }
 
 }
